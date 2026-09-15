13e0078826af7256fdfd41be9047ac9047bf67b9
DCWilsprof <devin.wilson0801@gmail.com>
2026-09-14 22:34:13 -0400

    javascript page 1

---
+++ js1.js
@@ -0,0 +1,12 @@
+console.log("this is javascript page 1");
+
+/**
+ * My name is Devin Wilson
+ * My favorite color is blue
+ * */
+
+let cookies = 5;
+const people = 2;
+console.log(cookies - people);
+cookies = 3;
+console.log("there are currently " + cookies + " cookies left");
